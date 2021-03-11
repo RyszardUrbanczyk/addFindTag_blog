b62a17e42c58c70fa816a1769e1fe1b4edb678d6
ryszard_urbanczyk <kontakt@grafitura.pl>
2021-03-11 12:15:02 +0100

    Create model: Program

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Program(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
